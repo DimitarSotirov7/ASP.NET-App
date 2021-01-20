134488b53b4a5d7b9f0f4660b0743c428fdbf5d2
DimitarSotirov7 <dimitarsotirov7@gmail.com>
2021-01-20 15:30:10 +0200

    Add Data DbContext

---
+++ src/Application.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt)
+            :base(opt)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder opt)
+        {
+            if (!opt.IsConfigured)
+            {
+                opt.UseSqlServer("Server=.;Database=ApplicationDb;Integrated Security=true;");
+            }
+        }
+    }
+}
